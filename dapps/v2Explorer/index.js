dc71bd8b23dd093b1ab81579185103cabb69fbdd
ignaciosantise <ignacio.santise@gmail.com>
2023-04-18 13:22:30 -0300

    fix: Added Buffer polyfill again

--- dapps/v2Explorer/index.js
+++ dapps/v2Explorer/index.js
@@ -6,4 +6,9 @@ import {AppRegistry} from 'react-native';
 import App from './src/screens/App';
 import {name as appName} from './app.json';
 
+// Polyfill Buffer
+if (typeof Buffer === 'undefined') {
+  global.Buffer = require('buffer').Buffer;
+}
+
 AppRegistry.registerComponent(appName, () => App);
