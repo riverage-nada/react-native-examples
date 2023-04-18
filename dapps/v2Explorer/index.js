4b3252a6da17c904680eb8058a56b94690a68eb3
ignaciosantise <ignacio.santise@gmail.com>
2023-04-18 10:38:09 -0300

    installed uprovider canary version

--- dapps/v2Explorer/index.js
+++ dapps/v2Explorer/index.js
@@ -6,28 +6,4 @@ import {AppRegistry} from 'react-native';
 import App from './src/screens/App';
 import {name as appName} from './app.json';
 
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-
-// Polyfill Buffer
-if (typeof Buffer === 'undefined') {
-  global.Buffer = require('buffer').Buffer;
-}
-
-// window.open = uri => {
-//   Linking.openURL(uri);
-// };
-
-// Polyfill localStorage
-// window.localStorage = {
-//   getItem: async key => {
-//     console.log('llamada');
-//     const value = await AsyncStorage.getItem(key);
-//     console.log('getItem', key, value);
-//     return value;
-//   },
-//   setItem: (key, value) => {
-//     AsyncStorage.setItem(key, JSON.stringify(value));
-//   },
-// };
-
 AppRegistry.registerComponent(appName, () => App);
